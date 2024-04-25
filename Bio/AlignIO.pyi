f4f28b6c0cc6a5454da08e5a59efe7f97aa71380
Ernesto Rodriguez <ernesto.rodriguez@tum.de>
2024-04-25 14:41:05 +0000

    [biopython] Typings for indexing Seq

--- Bio/AlignIO.pyi
+++ Bio/AlignIO.pyi
@@ -1,8 +1,10 @@
 from Bio.Align import MultipleSeqAlignment
-from typing import Iterable, Literal, TextIO, Union
+from typing import Iterable, Literal, Optional, TextIO, Union
 
 Handle = Union[str, TextIO]
 
 AlignFormat = Union[Literal['fasta'], Literal['clustal']]
 
 def write(alignments: Union[MultipleSeqAlignment, Iterable[MultipleSeqAlignment]], handle: Handle, format: AlignFormat) -> int: ...
+
+def read(handle: Handle, format: str,  seq_count: Optional[int] = None) ->  MultipleSeqAlignment: ...
