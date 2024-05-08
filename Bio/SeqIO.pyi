ed93b8baafa9d531a3267d8210ff98197df903a5
Ernesto Rodriguez <ernesto.rodriguez@tum.de>
2024-05-08 14:09:04 +0000

    [Biopython] Add parse and load for SeqIO

--- Bio/SeqIO.pyi
+++ Bio/SeqIO.pyi
@@ -1,8 +1,12 @@
 from Bio.SeqRecord import SeqRecord
-from typing import Iterable, TextIO, Union
+from typing import Iterator, Iterable, TextIO, Union
 
 def write(
     sequences: Union[Iterable[SeqRecord], SeqRecord],
     handle: TextIO,
     format: str,
 ) -> int: ...
+
+def parse(handle: TextIO, format: str) -> Iterator[SeqRecord]: ...
+
+def read(handle: TextIO, format: str) -> SeqRecord: ...
